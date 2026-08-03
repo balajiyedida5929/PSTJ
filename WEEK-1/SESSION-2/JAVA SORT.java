173ddd9e3439a97f4e4b28644d9f465e145ae04a
balajiyedida5929 <vtu28802@veltech.edu.in>
2026-08-03 11:08:41 +0530

    Create JAVA SORT.java

---
+++ WEEK-1/SESSION-2/JAVA SORT.java
@@ -0,0 +1,91 @@
+// PROGRAM
+
+import java.util.*;
+
+class Student {
+    private int id;
+    private String fname;
+    private double cgpa;
+
+    public Student(int id, String fname, double cgpa) {
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<>();
+
+        while (testCases > 0) {
+            int id = in.nextInt();
+            String fname = in.next();
+            double cgpa = in.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
+        }
+
+        Collections.sort(studentList, new Comparator<Student>() {
+            @Override
+            public int compare(Student s1, Student s2) {
+
+                // Sort by CGPA (Descending)
+                if (s1.getCgpa() != s2.getCgpa()) {
+                    return Double.compare(s2.getCgpa(), s1.getCgpa());
+                }
+
+                // Sort by Name (Ascending)
+                if (!s1.getFname().equals(s2.getFname())) {
+                    return s1.getFname().compareTo(s2.getFname());
+                }
+
+                // Sort by ID (Ascending)
+                return Integer.compare(s1.getId(), s2.getId());
+            }
+        });
+
+        for (Student st : studentList) {
+            System.out.println(st.getFname());
+        }
+
+        in.close();
+    }
+}
+
+
+
+// INPUT
+5
+33 Rumpa 3.68
+85 Ashis 3.85
+56 Samiha 3.75
+19 Samara 3.75
+22 Fahim 3.76
+
+// OUTPUT
+  Ashis
+Fahim
+Samara
+Samiha
+Rumpa
