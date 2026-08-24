781bfe5f2132fb563732b279301bcc95c7fd6979
balajiyedida5929 <vtu28802@veltech.edu.in>
2026-08-24 11:53:11 +0530

    Create Top K Frequent Elements.java

---
+++ 08-08-2026/WEEK-2/Session-9/Top K Frequent Elements.java
@@ -0,0 +1,59 @@
+// program:
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+       
+        Map<Integer, Integer> frequency = new HashMap<>();
+
+        for (int num : nums) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
+        }
+
+       
+        List<Integer>[] buckets = new List[nums.length + 1];
+
+        for (int num : frequency.keySet()) {
+            int freq = frequency.get(num);
+
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+
+            buckets[freq].add(num);
+        }
+
+       
+        int[] result = new int[k];
+        int index = 0;
+
+        for (int freq = buckets.length - 1; freq >= 0 && index < k; freq--) {
+            if (buckets[freq] != null) {
+                for (int num : buckets[freq]) {
+                    result[index] = num;
+                    index++;
+
+                    if (index == k) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+
+Accepted
+Runtime: 0 ms
+Case 1
+Case 2
+Case 3
+Input
+nums =
+[1,1,1,2,2,3]
+k =
+2
+Output
+[1,2]
+Expected
+[1,2]
