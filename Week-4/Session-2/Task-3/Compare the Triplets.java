5f8ecff9c3ef817ea8fab0477265514cc1a06e20
balajiyedida5929 <vtu28802@veltech.edu.in>
2026-09-21 11:52:10 +0530

    Create Compare the Triplets.java

---
+++ Week-4/Session-2/Task-3/Compare the Triplets.java
@@ -0,0 +1,63 @@
+// Program
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.*;
+
+class Result {
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int alice = 0;
+        int bob = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (a.get(i) > b.get(i)) {
+                alice++;
+            } else if (a.get(i) < b.get(i)) {
+                bob++;
+            }
+        }
+
+        return Arrays.asList(alice, bob);
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        BufferedWriter bufferedWriter = new BufferedWriter(
+            new FileWriter(System.getenv("OUTPUT_PATH"))
+        );
+
+        List<Integer> a = Stream.of(
+            bufferedReader.readLine().trim().split(" ")
+        ).map(Integer::parseInt).collect(toList());
+
+        List<Integer> b = Stream.of(
+            bufferedReader.readLine().trim().split(" ")
+        ).map(Integer::parseInt).collect(toList());
+
+        List<Integer> result = Result.compareTriplets(a, b);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+        );
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+Input (stdin)
+5 6 7
+3 6 10
+Your Output (stdout)
+1 1
+Expected Output
+1 1
